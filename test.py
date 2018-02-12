1d006b4e69c902eecc6c56eea8b1eaba1760dcd6
Marc Pulido <map@MacBook-Pro.local>
2018-02-12 11:28:13 -0500

    test.py for Python Beyond Scripts course

---
+++ test.py
@@ -0,0 +1,6 @@
+#!/Users/map/anaconda3/bin/python
+## So this is being coded on a Mac w/ Anaconda 3 installed.
+## Hence we are poining to ${HOME}/anaconda3/bin/python
+## Change as needed
+
+print( "hello anaconda")
